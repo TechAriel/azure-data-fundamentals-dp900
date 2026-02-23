8f2e798e79146f900b10babd3385a4bbe63930b5
Gabriel Oduor <91619174+TechAriel@users.noreply.github.com>
2026-02-23 20:26:37 +0100

    Update queries.sql

--- 01-azure-sql-database-relational-exploration/queries.sql
+++ 01-azure-sql-database-relational-exploration/queries.sql
@@ -1,1 +1,31 @@
+/*
+Project: Azure SQL Database – Relational Data Exploration
+Dataset: AdventureWorks
+Service: Azure SQL Database
+*/
 
+-- 1. Retrieve all columns from Product table
+SELECT *
+FROM SalesLT.Product;
+
+--------------------------------------------------------
+
+-- 2. Retrieve selected columns
+SELECT 
+    ProductID,
+    Name,
+    ListPrice,
+    ProductCategoryID
+FROM SalesLT.Product;
+
+--------------------------------------------------------
+
+-- 3. Join Product table with ProductCategory table
+SELECT 
+    p.ProductID,
+    p.Name AS ProductName,
+    c.Name AS Category,
+    p.ListPrice
+FROM SalesLT.Product AS p
+INNER JOIN SalesLT.ProductCategory AS c
+    ON p.ProductCategoryID = c.ProductCategoryID;
